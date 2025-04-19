71e306d2d07900dff519baf0890dadd692f0451c
sdp632 <sdp6@tpu.ru>
2025-04-19 14:50:47 +0800

    Add recipes app with models and views

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+from .models import (
+    Favorite,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    ShoppingCart
+)
+
+
+@register(Ingredient)
+class IngredientAdmin(ModelAdmin):
+    list_display = ("pk", "name", "measurement_unit")
+    search_fields = ("name",)
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+    min_num = 1
+    max_num = 1000
+    fields = ('ingredient', 'amount')
+
+
+@register(Recipe)
+class RecipeAdmin(ModelAdmin):
+    list_display = ("pk", "name", "author", "get_favorites", "created")
+    list_filter = ("author", "name")
+    search_fields = ("name", "author__username")
+    inlines = [RecipeIngredientInline]
+
+    @admin.display(description="Количество добавлений рецепта в избранное")
+    def get_favorites(self, obj):
+        return obj.favorites.count()
+
+
+@register(RecipeIngredient)
+class RecipeIngredientAdmin(ModelAdmin):
+    list_display = ("pk", "recipe", "ingredient", "amount")
+
+
+@register(ShoppingCart)
+class ShoppingCartAdmin(ModelAdmin):
+    list_display = ("pk", "user", "recipe")
+
+
+@register(Favorite)
+class FavoriteAdmin(ModelAdmin):
+    list_display = ("pk", "user", "recipe")
