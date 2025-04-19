3a34363c756dc1a60477a67f9d7a3e4095b6c849
sdp632 <sdp6@tpu.ru>
2025-04-19 14:50:10 +0800

    Add users app with authentication

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import Follow
+
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'recipes_count', 'subscribers_count')
+    search_fields = ('username', 'email')
+
+    @admin.display(description='Количество рецептов')
+    def recipes_count(self, obj):
+        return obj.recipes.count()
+
+    @admin.display(description='Количество подписчиков')
+    def subscribers_count(self, obj):
+        return obj.follower.count()
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    list_filter = ('user', 'following')
+    search_fields = ('user__username', 'following__username')
+    raw_id_fields = ('user', 'following')
+
+    def __str__(self):
+        return f'{self.user} подписчик автора - {self.following}'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,84 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+from foodgram.constants import (
+    USER_AVATAR_UPLOAD_TO,
+    USER_EMAIL_MAX_LENGTH,
+    USER_FIRST_NAME_MAX_LENGTH,
+    USER_LAST_NAME_MAX_LENGTH,
+    USER_USERNAME_MAX_LENGTH,
+    USER_USERNAME_REGEX
+)
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    email = models.EmailField(
+        verbose_name="Электронная почта",
+        max_length=USER_EMAIL_MAX_LENGTH,
+        unique=True,
+        error_messages={
+            'unique': 'Пользователь с таким email уже существует.',
+        }
+    )
+    username = models.CharField(
+        verbose_name="Имя пользователя",
+        max_length=USER_USERNAME_MAX_LENGTH,
+        unique=True,
+        db_index=True,
+        validators=[RegexValidator(regex=USER_USERNAME_REGEX)],
+    )
+    first_name = models.CharField(
+        verbose_name="Имя",
+        max_length=USER_FIRST_NAME_MAX_LENGTH,
+    )
+    last_name = models.CharField(
+        verbose_name="Фамилия",
+        max_length=USER_LAST_NAME_MAX_LENGTH,
+    )
+    avatar = models.ImageField(
+        verbose_name="Аватар пользователя",
+        upload_to=USER_AVATAR_UPLOAD_TO,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ("username",)
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="follower",
+        verbose_name="Подписчик",
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following",
+        verbose_name="Автор",
+    )
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "following"],
+                name="unique_user_following"
+            )
+        ]
+        ordering = ("user",)
+
+    def __str__(self):
+        return f"{self.user} подписчик автора - {self.following}"
